3eaa27fe0f496e3acfe0dbfa178600055ff2c903
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2023-05-20 19:07:36 -0400

    fix: getting logger

--- terrahaxs_runner/api.py
+++ terrahaxs_runner/api.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI, Request, HTTPException
 from terrahaxs_runner.settings import settings
 from terrahaxs_runner.models import Payload
 from terrahaxs_runner.runner import runner
-from terrahaxs_runner.logger import logger
+from terrahaxs_runner.logger import get as get_logger
+
+logger = get_logger()
 
 app = FastAPI()
 
