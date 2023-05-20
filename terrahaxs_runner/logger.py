9112cc79fa4a290ac49e00ef3932cf88957cbbd8
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2023-05-20 18:28:51 -0400

    remove prints

--- terrahaxs_runner/logger.py
+++ terrahaxs_runner/logger.py
@@ -4,9 +4,7 @@ from terrahaxs_runner.settings import settings
 def get():
     # Note: For some reason logging in GitHub Actions doesn't work unless
     # you use the print statement
-    print('calling get')
     if os.getenv('GITHUB_ACTIONS') is not None: # pragma: no cover
-        print('using custom logger')
         class Logger:
             def __init__(self):
                 pass
@@ -19,5 +17,4 @@ def get():
         return Logger()
     else:
         from aws_lambda_powertools import Logger
-        print('using aws lambda powertools logger')
         return Logger()
