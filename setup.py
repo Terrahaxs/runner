60a4feb54945ba1d4fe7363944d0beddd67bd0bd
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2023-05-27 13:27:33 -0400

    version: upgrade to version 0.0.14

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="terrahaxs-runner",
-    version="0.0.13",
+    version="0.0.14",
     description="Terrahaxs runner to execute Terraform CI/CD commands.",
     packages=find_packages(exclude=["features"]),
     include_package_data=True,
